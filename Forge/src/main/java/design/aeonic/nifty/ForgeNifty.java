c38fc5e5ef479d2c73f1148f2179c56f275e757f
aeonic <ericknwash@gmail.com>
2022-05-18 01:37:48 -0500

    Forge Aspects rewrite

--- Forge/src/main/java/design/aeonic/nifty/ForgeNifty.java
+++ Forge/src/main/java/design/aeonic/nifty/ForgeNifty.java
@@ -1,19 +1,16 @@
 package design.aeonic.nifty;
 
-import design.aeonic.nifty.api.aspect.Aspect;
-import design.aeonic.nifty.api.aspect.AspectProvider;
 import design.aeonic.nifty.api.aspect.internal.item.ItemHandler;
-import design.aeonic.nifty.api.core.Constants;
+import design.aeonic.nifty.api.util.Constants;
 import design.aeonic.nifty.impl.aspect.ForgeAspects;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.BlockEvent;
+import design.aeonic.nifty.impl.aspect.internal.item.ForgeItemHandler;
+import design.aeonic.nifty.impl.aspect.internal.item.IItemHandlerWrapper;
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.server.ServerLifecycleHooks;
+import net.minecraftforge.items.CapabilityItemHandler;
 
 @Mod(Constants.NIFTY_ID)
 @Mod.EventBusSubscriber(modid = Constants.NIFTY_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -24,17 +21,11 @@ public class ForgeNifty {
     public ForgeNifty() {
         Nifty.init();
 
-        MinecraftForge.EVENT_BUS.addListener((BlockEvent.NeighborNotifyEvent event) -> {
-            ServerPlayer player = ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers().get(0);
-            BlockHitResult result = ((BlockHitResult) player.pick(5, 0, true));
-            BlockEntity be = player.level.getBlockEntity(result.getBlockPos().below());
-            if (be == null) return;
+        // Capability -> Aspect wrapping and vice-versa
+        ASPECTS.registerWrappedCapability(ItemHandler.class, CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, IItemHandlerWrapper::new, ForgeItemHandler::new);
 
-            Aspect<ItemHandler> aspect1 = AspectProvider.cast(be).getAspect(ItemHandler.class, null, false);
-            String msg = aspect1.ifPresent(i -> i.getSlot(0).get().toString());
-            player.displayClientMessage(new TextComponent(msg == null ? "null" : msg), false);
-        });
-
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(ASPECTS.registerAspects);
+        // Event listeners
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(EventPriority.LOWEST, ASPECTS::processAspectCaps);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener((FMLClientSetupEvent event) -> Nifty.clientInit());
     }
 }
--- Forge/src/main/java/design/aeonic/nifty/mixin/ForgeBlockEntityMixin.java
+++ Forge/src/main/java/design/aeonic/nifty/mixin/ForgeBlockEntityMixin.java
@@ -1,66 +1,30 @@
 package design.aeonic.nifty.mixin;
 
-import design.aeonic.nifty.impl.aspect.ForgeAspectProvider;
+import design.aeonic.nifty.ForgeNifty;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
-import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Mixin(BlockEntity.class)
-public abstract class ForgeBlockEntityMixin extends CapabilityProvider<BlockEntity> implements ForgeAspectProvider<BlockEntity> {
+public class ForgeBlockEntityMixin extends CapabilityProvider<BlockEntity> {
 
-    private final List<Runnable> onRefresh = new ArrayList<>();
-    private final Map<Capability<?>, LazyOptional<?>> capabilityCache = new HashMap<>();
-
-    // Just here so we can compile
-    protected ForgeBlockEntityMixin(Class<BlockEntity> baseClass) {
+    protected ForgeBlockEntityMixin(Class baseClass) {
         super(baseClass);
     }
 
-    @Override
-    public BlockEntity self() {
-        return (BlockEntity) ((Object) this);
-    }
-
-    @Override
-    public void onRefreshAspects(Runnable callback) {
-        onRefresh.add(callback);
-    }
-
-    @Override
-    public void refreshAspects() {
-        onRefresh.forEach(Runnable::run);
-    }
-
-    @Override
-    public void invalidateCaps() {
-        refreshAspects();
-        super.invalidateCaps();
-    }
-
-    @Override
-    public void setDirty() {
-        LogManager.getLogger().info("changed");
-        self().setChanged();
-    }
-
-    @SuppressWarnings("unchecked")
     @Nonnull
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-        // Forge Capability contract expects us to cache these
-        @Nullable LazyOptional<T> maybeCap = (LazyOptional<T>) capabilityCache.computeIfAbsent(cap, c -> ForgeAspectProvider.getCapabilityCallback(this, c, side));
-        if (maybeCap != null) return maybeCap;
+        LazyOptional<T> opt = ForgeNifty.ASPECTS.queryAsCap((CapabilityProvider<?>) ((Object) this), cap, side);
+        if (opt != null)
+            return opt;
         return super.getCapability(cap, side);
     }
+
 }
