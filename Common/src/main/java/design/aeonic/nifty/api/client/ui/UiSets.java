6d2b7d71a1d6e6a90bd36c40852777785cb677c6
aeonic <ericknwash@gmail.com>
2022-05-28 22:42:49 -0500

    Energy UI elements and uv fixes for vanilla UI set

--- Common/src/main/java/design/aeonic/nifty/api/client/ui/UiSets.java
+++ Common/src/main/java/design/aeonic/nifty/api/client/ui/UiSets.java
@@ -1,5 +1,6 @@
 package design.aeonic.nifty.api.client.ui;
 
+import design.aeonic.nifty.api.client.ui.template.EnergyUiElementTemplate;
 import design.aeonic.nifty.api.client.ui.template.FillingUiElementTemplate;
 import design.aeonic.nifty.api.client.ui.template.StaticUiElementTemplate;
 import design.aeonic.nifty.api.client.ui.template.TankUiElementTemplate;
@@ -21,95 +22,140 @@ public final class UiSets {
          */
         public static final Texture TEXTURE_MAP = new Texture(new ResourceLocation(Constants.NIFTY_ID, "textures/gui/template/vanilla.png"));
 
+        // Boring stuff
+
         /**
          * A Screen background with the same size as most vanilla menu screens, blank except for player inventory slots.
          */
-        public static final StaticUiElementTemplate SCREEN_BACKGROUND = new StaticUiElementTemplate(TEXTURE_MAP, 176, 165, 80, 91);
+        public static final StaticUiElementTemplate BACKGROUND = new StaticUiElementTemplate(TEXTURE_MAP, 176, 166, 80, 90);
+
+        // Item slots
 
         /**
          * A normal item slot.
          */
-        public static StaticUiElementTemplate ITEM_SLOT_NORMAL = new StaticUiElementTemplate(TEXTURE_MAP, 18, 18, 0, 108);
+        public static final StaticUiElementTemplate ITEM_SLOT_NORMAL = new StaticUiElementTemplate(TEXTURE_MAP, 18, 18, 0, 108);
 
         /**
          * An output slot used in vanilla furnaces etc.
          */
-        public static StaticUiElementTemplate ITEM_SLOT_OUTPUT = new StaticUiElementTemplate(TEXTURE_MAP, 26, 26, 18, 108);
+        public static final StaticUiElementTemplate ITEM_SLOT_OUTPUT = new StaticUiElementTemplate(TEXTURE_MAP, 26, 26, 18, 108);
+
+        // Tanks
 
         /**
          * A tank the size of a normal inventory slot.
          */
-        public static TankUiElementTemplate TANK_ONE_ONE = new TankUiElementTemplate(TEXTURE_MAP,
-                18, 18, 126, 0, 63, 72, false);
+        public static final TankUiElementTemplate TANK_ONE_ONE = new TankUiElementTemplate(TEXTURE_MAP,
+                18, 18, 126, 0, 44, 108, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
 
         /**
          * A tank two slots high and the width of half a slot.
          */
-        public static TankUiElementTemplate TANK_TWO_HALF = new TankUiElementTemplate(TEXTURE_MAP,
-                9, 36, 126, 0, 0, 72, false);
+        public static final TankUiElementTemplate TANK_TWO_HALF = new TankUiElementTemplate(TEXTURE_MAP,
+                9, 36, 126, 0, 0, 72, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
 
         /**
          * A tank two slots high and the width of one slot.
          */
-        public static TankUiElementTemplate TANK_TWO_ONE = new TankUiElementTemplate(TEXTURE_MAP,
-                18, 36, 126, 0, 9, 72, false);
+        public static final TankUiElementTemplate TANK_TWO_ONE = new TankUiElementTemplate(TEXTURE_MAP,
+                18, 36, 126, 0, 9, 72, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
 
         /**
          * A tank two slots high and wide.
          */
-        public static TankUiElementTemplate TANK_TWO_TWO = new TankUiElementTemplate(TEXTURE_MAP,
-                36, 36, 126, 0, 27, 72, false);
+        public static final TankUiElementTemplate TANK_TWO_TWO = new TankUiElementTemplate(TEXTURE_MAP,
+                36, 36, 126, 0, 27, 72, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
 
         /**
          * A tank four slots high and the width of one slot.
          */
-        public static TankUiElementTemplate TANK_FOUR_ONE = new TankUiElementTemplate(TEXTURE_MAP,
-                18, 72, 126, 0, 0, 0, false);
+        public static final TankUiElementTemplate TANK_FOUR_ONE = new TankUiElementTemplate(TEXTURE_MAP,
+                18, 72, 126, 0, 0, 0, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
 
         /**
          * A tank four slots high and the width of two slots.
          */
-        public static TankUiElementTemplate TANK_FOUR_TWO = new TankUiElementTemplate(TEXTURE_MAP,
-                36, 72, 126, 0, 27, 0, false);
+        public static final TankUiElementTemplate TANK_FOUR_TWO = new TankUiElementTemplate(TEXTURE_MAP,
+                36, 72, 126, 0, 27, 0, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
 
         /**
          * A tank four slots high and wide.
          */
-        public static TankUiElementTemplate TANK_FOUR_FOUR = new TankUiElementTemplate(TEXTURE_MAP,
-                72, 72, 126, 0, 27, 0, false);
+        public static final TankUiElementTemplate TANK_FOUR_FOUR = new TankUiElementTemplate(TEXTURE_MAP,
+                72, 72, 126, 0, 54, 0, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
+
+        // Energy
+
+        /**
+         * An energy meter four slots high and the width of half a slot.
+         */
+        public static final EnergyUiElementTemplate ENERGY_FOUR_HALF = new EnergyUiElementTemplate(TEXTURE_MAP,
+                9, 72, 126, 0, 198, 0, 238, 0,
+                new float[]{.33f, 1f, .63f, 1f}, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
+
+        /**
+         * An energy meter four slots high and a quarter of a slot wide (ish).
+         */
+        public static final EnergyUiElementTemplate ENERGY_FOUR_QUARTER = new EnergyUiElementTemplate(TEXTURE_MAP,
+                5, 72, 126, 0, 207, 0, 238, 0,
+                new float[]{.33f, 1f, .63f, 1f}, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
+
+        /**
+         * An energy meter two slots high and the width of half a slot.
+         */
+        public static final EnergyUiElementTemplate ENERGY_TWO_HALF = new EnergyUiElementTemplate(TEXTURE_MAP,
+                9, 36, 126, 0, 212, 0, 247, 0,
+                new float[]{.33f, 1f, .63f, 1f}, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
+
+        /**
+         * An energy meter two slots high and a quarter of a slot wide (ish).
+         */
+        public static final EnergyUiElementTemplate ENERGY_TWO_QUARTER = new EnergyUiElementTemplate(TEXTURE_MAP,
+                5, 36, 126, 0, 221, 0, 247, 0,
+                new float[]{.33f, 1f, .63f, 1f}, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
+
+        /**
+         * An energy meter the height of a quarter slot (ish) and as wide as the player inventory slots (9 slots).
+         */
+        public static final EnergyUiElementTemplate ENERGY_QUARTER_NINE = new EnergyUiElementTemplate(TEXTURE_MAP,
+                162, 5, 63, 82, 63, 72, 63, 77,
+                new float[]{.33f, 1f, .63f, 1f}, FillingUiElementTemplate.FillDirection.LEFT_TO_RIGHT);
+
+        // Miscellaneous
 
         /**
          * The static recipe arrow used in vanilla guis such as the anvil.
          */
-        public static StaticUiElementTemplate RECIPE_ARROW = new StaticUiElementTemplate(TEXTURE_MAP, 23, 16, 0, 176);
+        public static final StaticUiElementTemplate RECIPE_ARROW = new StaticUiElementTemplate(TEXTURE_MAP, 23, 16, 0, 176);
 
         /**
          * The static recipe arrow with a red X used in vanilla guis such as the anvil.
          */
-        public static StaticUiElementTemplate RECIPE_ARROW_DISALLOWED = new StaticUiElementTemplate(TEXTURE_MAP, 23, 16, 0, 192);
+        public static final StaticUiElementTemplate RECIPE_ARROW_DISALLOWED = new StaticUiElementTemplate(TEXTURE_MAP, 23, 16, 0, 192);
 
         /**
          * The recipe arrow used in vanilla furnaces, filling from left to right.
          */
-        public static FillingUiElementTemplate RECIPE_ARROW_FILLING = new FillingUiElementTemplate(TEXTURE_MAP,
+        public static final FillingUiElementTemplate<FillingUiElementTemplate.FillLevel> RECIPE_ARROW_FILLING = new FillingUiElementTemplate<>(TEXTURE_MAP,
                 23, 16, 0, 176, 23, 176, FillingUiElementTemplate.FillDirection.LEFT_TO_RIGHT);
 
         /**
          * The burn time indicator used in vanilla furnaces.
          */
-        public static FillingUiElementTemplate BURN_TIME_INDICATOR = new FillingUiElementTemplate(TEXTURE_MAP,
+        public static final FillingUiElementTemplate<FillingUiElementTemplate.FillLevel> BURN_TIME_INDICATOR = new FillingUiElementTemplate<>(TEXTURE_MAP,
                 14, 14, 0, 162, 14, 162, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
 
         /**
          * The bubble progress indicator used in vanilla brewing stands.
          */
-        public static FillingUiElementTemplate BUBBLE_PROGRESS_BAR = new FillingUiElementTemplate(TEXTURE_MAP,
+        public static final FillingUiElementTemplate<FillingUiElementTemplate.FillLevel> BUBBLE_PROGRESS_BAR = new FillingUiElementTemplate<>(TEXTURE_MAP,
                 10, 28, 0, 134, 10, 134, FillingUiElementTemplate.FillDirection.BOTTOM_TO_TOP);
 
         /**
          * The downward facing progress arrow used in vanilla brewing stands.
          */
-        public static FillingUiElementTemplate DOWNWARD_PROGESS_ARROW = new FillingUiElementTemplate(TEXTURE_MAP,
+        public static final FillingUiElementTemplate<FillingUiElementTemplate.FillLevel> DOWNWARD_PROGESS_ARROW = new FillingUiElementTemplate<>(TEXTURE_MAP,
                 8, 27, 21, 134, 29, 134, FillingUiElementTemplate.FillDirection.TOP_TO_BOTTOM);
 
     }
