8bea19ab916beb4815f17fd92cd4349a216e1729
aeonic <ericknwash@gmail.com>
2022-05-29 18:51:48 -0500

    Add null check to Fabric fluid sprite/color callback

--- Fabric/src/main/java/design/aeonic/nifty/impl/client/FabricRenderHelper.java
+++ Fabric/src/main/java/design/aeonic/nifty/impl/client/FabricRenderHelper.java
@@ -4,6 +4,7 @@ import design.aeonic.nifty.api.client.RenderHelper;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class FabricRenderHelper implements RenderHelper {
@@ -11,6 +12,7 @@ public class FabricRenderHelper implements RenderHelper {
     @Override
     public Pair<TextureAtlasSprite, Integer> getFluidSpriteAndColor(FluidState state) {
         var renderHandler = FluidRenderHandlerRegistry.INSTANCE.get(state.getType());
+        if (renderHandler == null) renderHandler = FluidRenderHandlerRegistry.INSTANCE.get(Fluids.WATER);
         return Pair.of(renderHandler.getFluidSprites(null, null, state)[0], renderHandler.getFluidColor(null, null, state));
     }
 
